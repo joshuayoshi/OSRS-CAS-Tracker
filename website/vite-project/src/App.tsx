85abbfca04913beef57f17f8c10f6237343fa7da
Joshua Smith <japowpow@gmail.com>
2024-02-14 17:08:17 -0800

    consolidated cas_list.json entries so it's not nested/grouped by difficulty anymore

--- website/vite-project/src/App.tsx
+++ website/vite-project/src/App.tsx
@@ -1,33 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import jsonData from './data/cas_list.json';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  function DisplayJson() { //eew
+    return (
+      <div>
+        <h2>JSON Data</h2>
+        <ul>
+          {Object.entries(jsonData).map(([key, value]) => (
+            <li key={key}>
+              <h3>Entry {key}</h3>
+              <ul>
+                {value.map((innerValue, index) => (
+                  <li key={index}>
+                    <ul>
+                      {innerValue.map((item, innerIndex) => (
+                        <li key={innerIndex}>{item}</li>
+                      ))}
+                      <br />
+                    </ul>
+                  </li>
+                ))}
+                <br />
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Vite + React</h2>
+
+      {DisplayJson()}
+
     </>
   )
 }
